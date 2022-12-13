7900cb7e7917dce99410f68a197d16709abd4675
김창규 <kimc980106@naver.com>
2022-12-13 17:07:06 +0900

    2일차: 1일차 미션 리뷰 및 SSL 적용
    그 외 SpringSecurityFilterChain 와 각 Filter 에 대해 학습

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -1,21 +1,17 @@
 package com.prgrms.devcourse.configures;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,24 +25,33 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
                 .and()
                 .rememberMe()
                 .rememberMeParameter("remember-me")
                 .tokenValiditySeconds(300)
-                .alwaysRemember(false)
-                .userDetailsService(userDetailsService)
+//                .alwaysRemember(false)
+                .and()
+                .requiresChannel()
+                .anyRequest().requiresSecure()
                 .and();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication().withUser("user")
-                .password(encoder.encode("user123"))
-                .roles("USER");
-        auth.inMemoryAuthentication().withUser("admin")
-                .password(encoder.encode("admin123")).
-                roles("ADMIN");
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+//                .password(encoder.encode("{noop}user123"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}admin123")
+//                .password(encoder.encode("{noop}admin123"))
+                .roles("ADMIN");
     }
 }
